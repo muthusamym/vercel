f34181209b9bd7341b58e6e2f06ff41ab4d33b74
m.muthusamy <m.muthusamymca@gmail.com>
2025-03-17 10:10:15 +0530

    footer text changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
 export default function Footer()
 {
-    return <div className='py-4 bg-secondary text-center'>@copy; All Rights Reserved By Muthu Dot 2025</div>
+    return <div className='py-4 bg-secondary text-center'>@ All Rights Reserved By Muthu Dot 2025</div>
 }
